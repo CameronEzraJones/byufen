b3095ffa1f729c1100e3c2326ee2cb9f3aaefb9f
cejon <cejones90@msn.com>
2017-06-30 17:09:03 -0600

    Initial commit

---
+++ src/main/java/com/example/byufen/ByufenApplication.java
@@ -0,0 +1,12 @@
+package com.example.byufen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByufenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByufenApplication.class, args);
+	}
+}
